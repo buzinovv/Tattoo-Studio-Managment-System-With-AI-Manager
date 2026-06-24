16f6e725015d5ddc609992601d7ddcf4adeefca4
Mykhailo <meshanagg@gmail.com>
2026-06-24 21:47:10 +0200

    Успешно реализованы и протестированы add_client и get_client

--- database/db_commands.py
+++ database/db_commands.py
@@ -1,4 +1,5 @@
 import asyncpg
+import asyncio
 import os
 from dotenv import load_dotenv
 
@@ -23,3 +24,23 @@ async def add_client(telegram_id: int, name: str):
     
     await conn.execute(query, telegram_id, name)
     await conn.close() # Обязательно закрываем, чтобы не забить лимит соединений
+    
+async def get_client(telegram_id: int):
+    conn = await asyncpg.connect(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME")
+    )
+    
+    # Мы ищем ИМЯ пользователя по его TELEGRAM_ID
+    query = "SELECT name FROM clients WHERE telegram_id = $1"
+    
+    # fetchrow вернет строку, если юзер есть, или None, если нет
+    row = await conn.fetchrow(query, telegram_id)
+    
+    await conn.close()
+    
+    if row:
+        return row['name']
+    return None
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import asyncio
-from database.db_commands import add_client
+from database.db_commands import add_client, get_client
 
 async def main():
-    # Пробуем добавить тестового юзера
+    # Сначала добавим
     await add_client(123456789, "Тестовый Пользователь")
-    print("Пользователь успешно добавлен в БД!")
+    
+    # Теперь попробуем найти
+    name = await get_client(123456789)
+    print(f"Найден пользователь: {name}")
 
 if __name__ == "__main__":
     asyncio.run(main())
